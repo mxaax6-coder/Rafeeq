8b8ef450fe471a1293e5ff10bdc91a5c1e7420ac
mxaax6-coder <mx.aax6@gmail.com>
2026-02-07 23:46:28 +0300

    Add files via upload

---
+++ HealthCareProject/app/src/main/java/com/example/healthcareproject/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.healthcareproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // implemented by Manal
+        Button startButton = findViewById(R.id.startButton);
+
+        // implemented by Manal
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // implemented by Manal
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
